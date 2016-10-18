f636cf42836b3a3fe4aaf0e57b420f74868604c6
wanpeng <wanpeng.liu@jpmorgan.com>
2016-10-18 11:33:07 -0400

    ngroute working

---
+++ js/repository/repository-module-controller.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    var mod = angular.module("repo-module");
+
+    mod.controller('RepositoryController', repositoryController);
+    function repositoryController($scope) {
+        
+        $scope.repos=[{"id":1, "name":"repo1"},{"id":2, "name":"repo2"}];
+
+        }
+    
+})();
---
+++ js/user/user-module-controller.js
@@ -0,0 +1,11 @@
+﻿(function () {
+    var mod = angular.module("user-module");
+
+    mod.controller('UserController', usrController);
+    function usrController($scope) {
+        
+        $scope.users=[{"id":1, "name":"john"},{"id":2, "name":"wanpeng"}];
+
+        
+    }
+})();
