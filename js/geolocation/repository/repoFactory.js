0ae9c12d67454be8a73efeff245e6f2ea00abc31
wanpeng <wanpeng.liu@jpmorgan.com>
2016-10-20 08:47:46 -0400

    add animation add ngAnimate to app.js download animate.css add angular animate to index after angular.js add class xxx to ng-view add css definition xxx to main.css

---
+++ js/geolocation/repository/repoFactory.js
@@ -0,0 +1,22 @@
+﻿(function () {
+    var mod = angular.module('repo-module');
+    mod.factory('repoFactory', repoFactory);
+
+    function repoFactory($http) {
+
+        return {
+
+            query: function (repo) {
+                console.log("repo factory - user search for " + repo);
+                var url = "https://api.github.com/search/repositories?q="
+               + repo + "+language:java&sort=stars&order=desc";
+                return $http.get(url);
+            }
+        }
+        
+    }
+
+    // https://api.github.com/search/users?q=repos:1&location:iceland
+    // 
+
+})();
---
+++ js/geolocation/repository/repository-module-controller.js
@@ -0,0 +1,21 @@
+﻿(function () {
+    var mod = angular.module("repo-module");
+
+    mod.controller('RepositoryController', repositoryController);
+    function repositoryController($scope, repoFactory) {
+
+        console.log($scope.showP);
+
+        $scope.searchRepo = function () {
+            console.log("repo search");
+            repoFactory.query($scope.reposearch).then(display);
+
+        }
+
+        function display(response) {
+            $scope.repos = response.data;
+            console.log($scope.showP);
+        }
+    }
+
+})();
